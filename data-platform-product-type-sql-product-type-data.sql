8641b48814437476898f31d7a208ca36b24a6279
maimeimow <134258119+maimeimow@users.noreply.github.com>
2023-07-13 12:18:51 +0900

    release

--- data-platform-product-type-sql-product-type-data.sql
+++ data-platform-product-type-sql-product-type-data.sql
@@ -1,6 +1,8 @@
 CREATE TABLE `data_platform_product_type_product_type_data`
 (
     `ProductType`           varchar(3) NOT NULL,
+    
     PRIMARY KEY (`ProductType`)
+
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4;

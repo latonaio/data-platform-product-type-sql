57a82851f6a43cc8515b04d85cce03baf908a8f8
maimeimow <134258119+maimeimow@users.noreply.github.com>
2023-07-25 12:38:28 +0900

    release

--- data-platform-product-type-sql-product-type-data.sql
+++ data-platform-product-type-sql-product-type-data.sql
@@ -1,7 +1,10 @@
 CREATE TABLE `data_platform_product_type_product_type_data`
 (
-    `ProductType`           varchar(3) NOT NULL,
-    
+    `ProductType`            varchar(3) NOT NULL,
+    `CreationDate`           date NOT NULL,
+    `LastChangeDate`         date NOT NULL,
+    `IsMarkedForDeletion`    tinyint(1) DEFAULT NULL,
+
     PRIMARY KEY (`ProductType`)
 
 ) ENGINE = InnoDB
--- data-platform-product-type-sql-product-type-text-data.sql
+++ data-platform-product-type-sql-product-type-text-data.sql
@@ -1,13 +1,16 @@
 CREATE TABLE `data_platform_product_type_product_type_text_data`
 (
-    `ProductType`                     varchar(3) NOT NULL,
-    `Language`                        varchar(2) NOT NULL,
-    `ProductTypeName`                 varchar(100) NOT NULL,
+    `ProductType`                  varchar(3) NOT NULL,
+    `Language`                     varchar(2) NOT NULL,
+    `ProductTypeName`              varchar(100) NOT NULL,
+    `CreationDate`                 date NOT NULL,
+    `LastChangeDate`               date NOT NULL,
+    `IsMarkedForDeletion`          tinyint(1) DEFAULT NULL,
     
     PRIMARY KEY (`ProductType`, `Language`),
 
-    CONSTRAINT `DataPlatformProductTypeProductTypeTextDataProductType_fk` FOREIGN KEY (`ProductType`) REFERENCES `data_platform_product_type_product_type_data` (`ProductType`),
-    CONSTRAINT `DataPlatformProductTypeProductTypeTextDataLanguage_fk` FOREIGN KEY (`Language`) REFERENCES `data_platform_language_language_data` (`Language`)
+    CONSTRAINT `DataPlatformProductTypeTextDataProductType_fk` FOREIGN KEY (`ProductType`) REFERENCES `data_platform_product_type_product_type_data` (`ProductType`),
+    CONSTRAINT `DataPlatformProductTypeTextDataLanguage_fk` FOREIGN KEY (`Language`) REFERENCES `data_platform_language_language_data` (`Language`)
 
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4;
